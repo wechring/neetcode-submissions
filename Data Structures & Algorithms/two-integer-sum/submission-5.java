bf1cb688ce1c07431dbd26fd45a2cfc46120013a
Wech Ring <136630208+wechring@users.noreply.github.com>
2026-08-05 21:41:14 -0400

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+     
+     HashMap<Integer, Integer> h = new HashMap<>();
+
+     for(int i = 0; i < nums.length; i++){
+        if(h.containsKey(target-nums[i])){
+            return new int[]{h.get(target-nums[i]), i};
+        } else{
+            h.put(nums[i],i);
+        }
+     }
+     return null;
+
+    }
+}
